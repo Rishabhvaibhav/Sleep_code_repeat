ee115356a9d2987419cee6b96169002d76579dbf
Rishabh Vaibhav <48403808+Rishabhvaibhav@users.noreply.github.com>
2023-08-22 10:28:21 +0530

    update

---
+++ PrintFriendsNames.py
@@ -0,0 +1,15 @@
+'''
+Write a program “PrintFriendsNames” that takes 3 friends’ names as input
+arguments and prints out “Hi” to these friends in the reverse of the order given. It
+also adds “and “ before the last name.
+For example:
+Input Arguments : Mahesh, Suresh, Devesh
+Output : “Hi Devesh, Suresh and Mahesh”
+'''
+
+
+a = "mahesh"
+b = "suresh"
+c = "Devesh"
+
+print(f(" + "Hii",{a},{b}, "and",{c}"))
